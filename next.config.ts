578b4e15ac5a8924bcb344f574779228512185ea
Ridhsuki <basukiridhoalghifary@gmail.com>
2025-07-24 22:06:05 +0700

    fix: undifined apikey

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  env: {
+    OPENROUTER_API_KEY: process.env.OPENROUTER_API_KEY,
+  },
 };
 
 export default nextConfig;
